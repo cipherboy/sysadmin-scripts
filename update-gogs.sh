350be0e6dc619aaba70f56b710679a017783cb7c
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-07-23 20:19:09 -0400

    Add gogs update script
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ update-gogs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+systemctl stop gogs
+
+gogs_url="$1"
+
+su git -c "cd ~git && wget \"$gogs_url\" -O linux_amd64.tar.gz && rm -rf tmp && mkdir tmp && cd tmp && tar -xf ../linux_amd64.tar.gz && cd ../ && rm -rf gogs.bak && cp -pr gogs gogs.bak && rm gogs/templates gogs/scripts gogs/public -r && cp tmp/gogs ./ -r"
+
+systemctl start gogs
