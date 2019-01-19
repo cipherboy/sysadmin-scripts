a2cf3bd1c22f1ac665ba915f6e0a923c0adffdf2
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-01-19 17:00:35 -0500

    Update renew-certs.sh
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- renew-certs.sh
+++ renew-certs.sh
@@ -2,7 +2,11 @@
 
 systemctl stop nginx
 
-cd /opt/letsencrypt
-./certbot-auto --standalone renew
+if [ -d "/opt/letsencrypt" ]; then
+    cd /opt/letsencrypt
+    ./certbot-auto --standalone renew
+elif command -v "certbot" 1>/dev/null; then
+    certbot --standalone renew
+fi
 
 systemctl start nginx
