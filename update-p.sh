fe89b5d0de2ccdce2eb857e1c2d1705070ca5136
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-02-19 16:34:32 -0500

    Add changelog to update-p
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- update-p.sh
+++ update-p.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-set -e
+update_p() {(
+    set -e
 
-if [ ! -e "$HOME/GitHub/cipherboy/p" ]; then
-    mkdir -p "$HOME/GitHub/cipherboy"
-    cd "$HOME/GitHub/cipherboy"
-    git clone https://github.com/cipherboy/p
-fi
+    if [ ! -e "$HOME/GitHub/cipherboy/p" ]; then
+        mkdir -p "$HOME/GitHub/cipherboy"
+        cd "$HOME/GitHub/cipherboy"
+        git clone https://github.com/cipherboy/p
+    fi
 
-cd "$HOME/GitHub/cipherboy/p"
-git pull || true
+    cd "$HOME/GitHub/cipherboy/p"
 
-mkdir -p bin && ./build.py
+    commit="$(git rev-parse HEAD)"
+    git pull || true
+    git log --color=always --oneline "$commit..HEAD" | cat -
 
-mkdir -p "$HOME/bin" || true
-cp bin/p "$HOME/bin/p"
+    mkdir -p bin && ./build.py
+
+    mkdir -p "$HOME/bin" || true
+    cp bin/p "$HOME/bin/p"
+)}
+
+update_p "@"
