f1475e6c203ab3159a2d1872d36b85faf85dd960
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-10-10 08:39:50 -0400

    upddate-p needs to reflect sharg dependency
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- update-p.sh
+++ update-p.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-update_p() {(
+function update_p() {(
     set -e
+
+    if [ ! -e "$HOME/GitHub/cipherboy/sharg" ]; then
+        mkdir -p "$HOME/GitHub/cipherboy"
+        cd "$HOME/GitHub/cipherboy"
+        git clone https://github.com/cipherboy/sharg
+    fi
 
     if [ ! -e "$HOME/GitHub/cipherboy/p" ]; then
         mkdir -p "$HOME/GitHub/cipherboy"
@@ -9,13 +15,23 @@ update_p() {(
         git clone https://github.com/cipherboy/p
     fi
 
+    # Update sharg first
+    cd "$HOME/GitHub/cipherboy/sharg"
+
+    local commit="$(git rev-parse HEAD)"
+    git pull || true
+    git log --color=always --oneline "$commit..HEAD" | cat -
+
+    pip3 install --user -e .
+
+    # Then update p
     cd "$HOME/GitHub/cipherboy/p"
 
     commit="$(git rev-parse HEAD)"
     git pull || true
     git log --color=always --oneline "$commit..HEAD" | cat -
 
-    mkdir -p bin && ./build.py
+    mkdir -p bin && python3 ./build.py
 
     mkdir -p "$HOME/bin" || true
     cp bin/p "$HOME/bin/p"
